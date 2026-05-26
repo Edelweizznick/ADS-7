46e2b7ae87187677c583a26462d3339568253bb6
Edelweizznick <kolia3003@yahoo.com>
2026-05-26 13:35:27 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+// Copyright 2026 NNTU-CS
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
@@ -8,10 +9,8 @@ int main() {
     std::srand(std::time(nullptr));
     std::ofstream outFile("result/data.csv");
     outFile << "n,ops_all_off,ops_all_on,ops_random" << std::endl;
-    
     for (int n = 10; n <= 500; n += 10) {
         int ops_off, ops_on, ops_random;
-        
         {
             Train train;
             for (int i = 0; i < n; i++) train.addCar(false);
@@ -26,12 +25,13 @@ int main() {
         }
         {
             Train train;
-            for (int i = 0; i < n; i++) train.addCar(std::rand() % 2);
+            for (int i = 0; i < n; i++)
+                train.addCar(std::rand() % 2);
             train.getLength();
             ops_random = train.getOpCount();
         }
-        
-        outFile << n << "," << ops_off << "," << ops_on << "," << ops_random << std::endl;
+        outFile << n << "," << ops_off << ","
+                << ops_on << "," << ops_random << std::endl;
     }
     outFile.close();
     return 0;
