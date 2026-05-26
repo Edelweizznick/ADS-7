912ac43dbea1fbdd49e7b9ece920fc54b98e1206
Edelweizznick <kolia3003@yahoo.com>
2026-05-26 17:09:41 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,36 +6,33 @@
 int main() {
   std::random_device rd;
   std::mt19937 gen(rd());
-  std::uniform_int_distribution<> dist(0, 1);
+  std::uniform_int_distribution<> dis(0, 1);
 
   std::cout << "n\tall_off\tall_on\trandom_avg\n";
 
   for (int n = 2; n <= 100; ++n) {
-    // Все выключены
     Train trainOff;
     for (int i = 0; i < n; ++i) trainOff.addCar(false);
     trainOff.getLength();
-    int off = trainOff.getOpCount();
+    int opsOff = trainOff.getOpCount();
 
-    // Все включены
     Train trainOn;
     for (int i = 0; i < n; ++i) trainOn.addCar(true);
     trainOn.getLength();
-    int on = trainOn.getOpCount();
+    int opsOn = trainOn.getOpCount();
 
-    // Случайное (усредняем по 10 запускам)
-    int sumRandom = 0;
+    int randomSum = 0;
     for (int trial = 0; trial < 10; ++trial) {
       Train trainRand;
       for (int i = 0; i < n; ++i) {
-        trainRand.addCar(dist(gen) == 1);
+        trainRand.addCar(dis(gen) == 1);
       }
       trainRand.getLength();
-      sumRandom += trainRand.getOpCount();
+      randomSum += trainRand.getOpCount();
     }
-    int randomAvg = sumRandom / 10;
+    int randomAvg = randomSum / 10;
 
-    std::cout << n << "\t" << off << "\t" << on << "\t" << randomAvg << "\n";
+    std::cout << n << "\t" << opsOff << "\t" << opsOn << "\t" << randomAvg << "\n";
   }
 
   std::cout << "\nЭксперимент завершён!\n";
