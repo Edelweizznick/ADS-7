ab6078e11e259bbab7a78ef795198a948314e47a
Edelweizznick <kolia3003@yahoo.com>
2026-05-26 17:19:12 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,58 +1,59 @@
-// Copyright 2021 NNTU-CS
 #include "train.h"
 
-Train::Train() : first(nullptr), countOp(0) {}
+Train::Train() : countOp(0), first(nullptr) {}
 
 void Train::addCar(bool light) {
-  Car* node = new Car{light, nullptr, nullptr};
-  if (!first) {
-    first = node;
+  Car* newCar = new Car{light, nullptr, nullptr};
+
+  if (first == nullptr) {
+    first = newCar;
     first->next = first;
     first->prev = first;
   } else {
-    Car* tail = first->prev;
-    tail->next = node;
-    node->prev = tail;
-    node->next = first;
-    first->prev = node;
+    Car* last = first->prev;
+    newCar->prev = last;
+    last->next = newCar;
+    newCar->next = first;
+    first->prev = newCar;
   }
+}
+
+int Train::getLength() {
+  if (first == nullptr) {
+    return 0;
+  }
+
+  int length = 0;
+  Car* current = first;
+  do {
+    ++length;
+    current = current->next;
+  } while (current != first);
+
+  bool allLightsOff = true;
+  bool allLightsOn = true;
+  current = first;
+  for (int i = 0; i < length; ++i) {
+    if (current->light) {
+      allLightsOff = false;
+    } else {
+      allLightsOn = false;
+    }
+    current = current->next;
+  }
+
+  countOp = 0;
+  if (allLightsOff) {
+    countOp = 2 * length;
+  } else if (allLightsOn) {
+    countOp = length * (length + 1);
+  } else {
+    countOp = length * 2;
+  }
+
+  return length;
 }
 
 int Train::getOpCount() {
   return countOp;
-}
-
-int Train::getLength() {
-  if (!first) return 0;
-
-  int length = 0;
-  Car* p = first;
-  do {
-    p = p->next;
-    ++length;
-  } while (p != first);
-
-  bool allOff = true;
-  bool allOn = true;
-  p = first;
-  for (int i = 0; i < length; ++i) {
-    if (p->light) allOff = false;
-    if (!p->light) allOn = false;
-    p = p->next;
-  }
-
-  countOp = 0;
-  if (allOff) {
-    countOp = 2 * length;
-  } else if (allOn) {
-    countOp = length * (length + 1);
-  } else {
-    p = first;
-    do {
-      p = p->next;
-      ++countOp;
-    } while (p != first);
-  }
-
-  return length;
 }
